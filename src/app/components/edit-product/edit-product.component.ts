ee691c0ae94ebbc5d1f2578762fa6c2b71d16e19
Sachin Gawade <59793481+saching3234@users.noreply.github.com>
2022-08-15 13:44:02 +0530

    Update edit-product.component.ts

--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -54,7 +54,7 @@ export class EditProductComponent implements OnInit {
  
   //method for getting the selected image
   onSelectedFile(event){
-    const file=event.target.files[0];
+    const file=event.target.files[0].name;
     this.userFile=file;
     console.log(this.userFile);
   }
