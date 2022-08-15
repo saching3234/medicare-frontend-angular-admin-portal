d1ce6cd14df52b7499a551f2a16d393b07c55bdc
Sachin Gawade <59793481+saching3234@users.noreply.github.com>
2022-08-15 11:52:06 +0530

    Update order-services.service.ts

--- src/app/services/order-services.service.ts
+++ src/app/services/order-services.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class OrderServicesService {
 
   private token;
-  private url="http://52.0.196.126:4201/api/admin/userOrders/";
+  private url="http://18.209.43.115:4201/api/admin/userOrders/";
   private header2;
   constructor(private http:HttpClient) {
     //getting the locall saved token
