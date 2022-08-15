a6974e7cd187ce566934aab7b0104083310df689
Sachin Gawade <59793481+saching3234@users.noreply.github.com>
2022-08-15 11:52:33 +0530

    Update product-services.service.ts

--- src/app/services/product-services.service.ts
+++ src/app/services/product-services.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProductServicesService {
-  private url="http://52.0.196.126:4201/api/admin/products/";
+  private url="http://18.209.43.115:4201/api/admin/products/";
   private token;
 
   constructor(private http:HttpClient) { 
