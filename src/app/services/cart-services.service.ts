5313d7c4209ac19ab63f9e794f399d9b3e196dc1
Sachin Gawade <59793481+saching3234@users.noreply.github.com>
2022-08-15 11:50:17 +0530

    Update cart-services.service.ts

--- src/app/services/cart-services.service.ts
+++ src/app/services/cart-services.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class CartServicesService {
   
-  private cartUrl="http://52.0.196.126:4201/api/users/cart";
-  private orderUrl="http://52.0.196.126:4201/api/users/orders"
+  private cartUrl="http://18.209.43.115:4201/api/users/cart";
+  private orderUrl="http://18.209.43.115:4201/api/users/orders"
 
   constructor(private http:HttpClient) { }
 
