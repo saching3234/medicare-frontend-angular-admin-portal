cb582b59e8b5615e3e58e4e9807e28501ec2f277
Sachin Gawade <59793481+saching3234@users.noreply.github.com>
2022-08-15 11:53:09 +0530

    Update user-services.service.ts

--- src/app/services/user-services.service.ts
+++ src/app/services/user-services.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserServicesService {
-  private url="http://52.0.196.126:4201/api/admin";
-  private url2="http://52.0.196.126:4201/api/users/change"
+  private url="http://18.209.43.115:4201/api/admin";
+  private url2="http://18.209.43.115:4201/api/users/change"
 
   constructor(private httpClient:HttpClient) { }
 
